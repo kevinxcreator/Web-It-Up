8695952c6d3e9d4d72414313880938d16e982403
Anish <anish.dhandore@gmail.com>
2021-04-17 22:50:50 +0530

    updated settings

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -131,10 +131,10 @@ STATIC_URL = '/static/'
 #     BASE_DIR / "static",
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 
 
